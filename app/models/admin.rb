7b5b93239418bca4e276f477af8f7af589517611
JasmineG <yo.kitchen@gmail.com>
2016-02-23 23:40:15 -0800

    login page works

---
+++ app/models/admin.rb
@@ -0,0 +1,8 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :omniauthable
+  # I enabled :confirmable, :lockable, :timeoutable, which are not defaults
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
+end
