f69e73100d303cf54366ddd20d999bcd5571137c
Kristen Law <kristenlaw.2014@gmail.com>
2016-02-28 11:33:03 -0800

    Add teacher to db table

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -5,7 +5,13 @@ class TeacherController < ApplicationController
   end
 
   def create
+    teacher = Teacher.new(params.permit(:name, :city, :state, :email, :phone,
+      :school, :communication_method, :grade, :topics, :class_times, :climate_change,
+      :evolution, :topics_to_cover, :benefits, :requirements, :read_expectations, :approved))
+    teacher.save
 
+    redirect_to '/teachers/new'
+    
   end
 
 end
