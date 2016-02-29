f357a653203fc1a88dca792c38c23cb20a5d5c5e
Kristen Law <kristenlaw.2014@gmail.com>
2016-02-28 20:26:14 -0800

    Update controllers to save correct information

--- app/controllers/scientist_controller.rb
+++ app/controllers/scientist_controller.rb
@@ -6,9 +6,45 @@ class ScientistController < ApplicationController
 
   def create
 
-    scientist = Scientist.new(params.permit(:name, :city, :state, :email, :phone, :communication_method,
-      :occupation, :education, :affiliation, :evolution, :climate_change, :specialization, :experience,
-      :goals, :availability, :committed, :less_than_30, :link, :read_expectations))
+    scientist = Scientist.new(params.permit(:name, :city, :state, :email, :phone, :education, :affiliation,
+      :specialization, :experience, :goals, :availability, :less_than_30, :link, :read_expectations))
+
+    if params[:communication_method] == "other"
+      scientist.communication_method = params[:communication_method_other]
+    else
+      scientist.communication_method = params[:communication_method]
+    end
+
+    if params[:occupation] == "other"
+      scientist.occupation = params[:occupation_other]
+    else
+      scientist.occupation = params[:occupation]
+    end
+
+    case params[:topics]
+    when "climate_change"
+      scientist.climate_change = true
+      scientist.evolution = false
+    when "evolution"
+      scientist.climate_change = false
+      scientist.evolution = true
+    when "both"
+      scientist.climate_change = true
+      scientist.evolution = true
+    end
+
+    if params[:committed]
+      scientist.committed = true
+    else
+      scientist.committed = false
+    end
+
+    if params[:read_expectations]
+      scientist.read_expectations = true
+    else
+      scientist.read_expectations = false
+    end
+
     scientist.save
 
     redirect_to '/scientists/new'
--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -5,13 +5,43 @@ class TeacherController < ApplicationController
   end
 
   def create
-    teacher = Teacher.new(params.permit(:name, :city, :state, :email, :phone,
-      :school, :communication_method, :grade, :topics, :class_times, :climate_change,
-      :evolution, :topics_to_cover, :benefits, :requirements, :read_expectations, :approved))
+    teacher = Teacher.new(params.permit(:name, :city, :state, :email, :phone, :school,
+      :grade, :topics, :class_times, :topics_to_cover, :benefits, :requirements))
+
+    if params[:communication_method] == "other"
+      teacher.communication_method = params[:communication_method_other]
+    else
+      teacher.communication_method = params[:communication_method]
+    end
+
+    case params[:topics]
+    when "climate_change"
+      teacher.climate_change = true
+      teacher.evolution = false
+    when "evolution"
+      teacher.climate_change = false
+      teacher.evolution = true
+    when "both"
+      teacher.climate_change = true
+      teacher.evolution = true
+    end
+
+    if params[:read_expectations]
+      teacher.read_expectations = true
+    else
+      teacher.read_expectations = false
+    end
+
+    if params[:approved]
+      teacher.approved = true
+    else
+      teacher.approved = false
+    end
+
     teacher.save
 
     redirect_to '/teachers/new'
-    
+
   end
 
 end
