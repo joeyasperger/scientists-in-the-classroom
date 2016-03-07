a9523f41ffcb01e330f2e3009b42f58e7c247491
joeyasperger <joeyasperger@gmail.com>
2016-03-07 00:02:12 -0800

    made nav bar do stuff

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,11 +1,13 @@
 class MatchController < ApplicationController
 
     def new
+        @page = 'match'
         @teachers = Teacher.all
         @scientists = Scientist.all
     end
 
     def index
+        @page = 'matches'
         @matches = Match.all
     end
 end
