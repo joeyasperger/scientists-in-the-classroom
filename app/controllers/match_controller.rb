ba4800f3648792dc652518c481d0c82d537b095c
JasmineG <yo.kitchen@gmail.com>
2016-03-09 23:40:55 -0800

    filtering works, just need to collapse things

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -8,37 +8,46 @@ class MatchController < ApplicationController
     def get_matched_scientists
     	curr_teacher = Teacher.find_by_id(params[:id])
     	array_of_ids = Array.new
-        far_scientists = Array.new
+        incompatible_scientists = Array.new
     	#default to 50 miles as distance for teachers
-    	for scientist in Scientist.within(50, :origin => [curr_teacher.lat, curr_teacher.lng]).all do
-    		if ((curr_teacher.climate_change && scientist.climate_change) || 
-                (curr_teacher.evolution && scientist.evolution)) then
+        distance = 50
+    	for scientist in Scientist.within(distance, :origin => [curr_teacher.lat, curr_teacher.lng]).all do
+    		if ((curr_teacher.climate_change == true && scientist.climate_change == true) || 
+                (curr_teacher.evolution == true && scientist.evolution == true)) then
                 array_of_ids << scientist.id
+            else
+                incompatible_scientists << scientist.id
             end
     	end
         for scientist in Scientist.beyond(50, :origin => [curr_teacher.lat, curr_teacher.lng]).all do
-            far_scientists << scientist.id
+            incompatible_scientists << scientist.id
         end
 
-    	data = {:scientists_in_range => array_of_ids, :scientists_out_of_range => far_scientists}
+    	data = {:scientists_in_range => array_of_ids, :incompatible_scientists => incompatible_scientists}
   		render :json => data, :status => :ok
 
     end
 
     def get_matched_teachers
     	curr_scientist = Scientist.find_by_id(params[:id])
-    	array_of_ids = Array.new
+        incompatible_teachers = Array.new
     	if curr_scientist.less_than_30 then
     		distance = 30
     	else
     		distance = 50
     	end
-    	for teacher in Scientist.within(distance, :origin => 
+    	for teacher in Teacher.within(distance, :origin => 
     							[curr_scientist.lat, curr_scientist.lng]).all do
-    		array_of_ids << teacher.id
+            if (!((curr_scientist.climate_change == true && teacher.climate_change == true) || 
+                (curr_scientist.evolution == true && teacher.evolution == true))) then
+                incompatible_teachers << teacher.id
+            end
     	end
-    	data = {:message => array_of_ids}
-  		render :json => data, :status => :ok
+        for teacher in Teacher.beyond(50, :origin => [curr_scientist.lat, curr_scientist.lng]).all do
+            incompatible_teachers << teacher.id
+        end
+        data = {:incompatible_teachers => incompatible_teachers}
+        render :json => data, :status => :ok
 
     end
 end
--- db/migrate/20160307065711_add_sample_people.rb
+++ db/migrate/20160307065711_add_sample_people.rb
@@ -3,14 +3,44 @@ class AddSamplePeople < ActiveRecord::Migration
   	teacher1 = Teacher.new(:name => "Jeff Rogers", :city => "Berkeley", 
   						:state => "California",
   						:email => "yo.kitchen@gmail.com", :phone => "1234567890",
-  						:school => "St. Mary's College High School", :lat => 27, :lng => -122,
-  						:communication_method => "email", :zip_code => "94705")
+  						:school => "St. Mary's College High School", :lat => 37.871399, :lng => -122.259591,
+  						:communication_method => "email", :zip_code => "94705", evolution: true,
+              climate_change: false)
     teacher1.save(:validate => false)
 
     scientist1 = Scientist.new(name: "Young Lee", city: "Berkeley", state:
-    						"California", email: "physicsisgreat@stanford.edu", :lat => 27, :lng => -122,
+    						"California", email: "physicsisgreat@stanford.edu", :lat => 37.871399, 
+                :lng => -122.259591, evolution: true, climate_change: false,
     						:zip_code => "94703")
     scientist1.save(:validate => false)
+
+    teacher2 = Teacher.new(:name => "Craig Sutphin", :city => "Houston", 
+              :state => "Texas",
+              :email => "yo.kitchen@gmail.com", :phone => "1234567890",
+              :school => "St. Mary's College High School", :lat => 29.742151, :lng => -95.397540,
+              :communication_method => "email", :zip_code => "77006", evolution: false,
+              climate_change: true)
+    teacher2.save(:validate => false)
+
+    scientist2 = Scientist.new(name: "John Forte", city: "Houston", state:
+                "Texas", email: "physicsisgreat@stanford.edu", :lat => 29.742151, 
+                :lng => -95.397540, evolution: false, climate_change: true,
+                :zip_code => "77006")
+    scientist2.save(:validate => false)
+
+    teacher3 = Teacher.new(:name => "Jennifer Jack", :city => "El Cerrito", 
+              :state => "California",
+              :email => "yo.kitchen@gmail.com", :phone => "1234567890",
+              :school => "St. Mary's College High School", :lat => 37.921409, :lng => -122.303532,
+              :communication_method => "email", :zip_code => "94530", evolution: false,
+              climate_change: true)
+    teacher3.save(:validate => false)
+
+    scientist3 = Scientist.new(name: "Kathleen Gonzalez", city: "Albany", state:
+                "California", email: "physicsisgreat@stanford.edu", :lat => 37.891490, 
+                :lng => -122.304575, evolution: true, climate_change: true,
+                :zip_code => "94706")
+    scientist3.save(:validate => false)
 
   end
 end
