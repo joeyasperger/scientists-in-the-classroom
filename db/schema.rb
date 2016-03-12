efca6f5396d154c7a0e43189bc0c186ed3145f10
JasmineG <yo.kitchen@gmail.com>
2016-03-12 15:56:28 -0800

    removed lockable feature

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,6 @@ ActiveRecord::Schema.define(version: 20160307065711) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
@@ -36,7 +33,6 @@ ActiveRecord::Schema.define(version: 20160307065711) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
 
   create_table "matches", force: :cascade do |t|
     t.integer  "scientist_id"
